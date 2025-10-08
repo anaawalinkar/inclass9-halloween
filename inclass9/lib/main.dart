dbfa06cb337f5e9a048dc647c437a9f65e72fcde
jaydenmoore3936 <jaydenmoore3936@gmail.com>
2025-10-07 23:03:28 -0400

    Update main.dart

--- inclass9/lib/main.dart
+++ inclass9/lib/main.dart
@@ -1,122 +1,437 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SpookyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SpookyApp extends StatefulWidget {
+  const SpookyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<SpookyApp> createState() => _SpookyAppState();
+}
+
+class _SpookyAppState extends State<SpookyApp> {
+  final AudioPlayer _bgPlayer = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    _playBackgroundMusic();
+  }
+
+  void _playBackgroundMusic() async {
+    await _bgPlayer.setReleaseMode(ReleaseMode.loop);
+    await _bgPlayer.setSource(AssetSource('audio/spooky_bg.mp3'));
+    await _bgPlayer.resume();
+  }
+
+  @override
+  void dispose() {
+    _bgPlayer.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Spooktacular Storybook',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF6A0DAD),
+          brightness: Brightness.dark,
+        ),
+        scaffoldBackgroundColor: const Color(0xFF1A1A2E),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF2C073A),
+        ),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TitleScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TitleScreen extends StatefulWidget {
+  const TitleScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TitleScreen> createState() => _TitleScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TitleScreenState extends State<TitleScreen> with TickerProviderStateMixin {
+  late AnimationController _fadeController;
+  late Animation<double> _fadeAnimation;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _fadeController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _fadeAnimation = Tween<double>(begin: 0.2, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _fadeController,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _fadeController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        color: const Color(0xFF1A1A2E),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'The Mystery of Midnight Alley',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFFFD700),
+                  shadows: [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.black,
+                      offset: Offset(3.0, 3.0),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              FadeTransition(
+                opacity: _fadeAnimation,
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const GameScreen(),
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.psychology_outlined),
+                  label: const Text('Start the Challenge', style: TextStyle(fontSize: 20)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF6A0DAD),
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Find the Lucky Bat to win!',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Find the Lucky Bat'),
+        backgroundColor: const Color(0xFF2C073A),
+      ),
+      body: const HauntedScreenLayout(),
+    );
+  }
+}
+
+class HauntedScreenLayout extends StatefulWidget {
+  const HauntedScreenLayout({super.key});
+
+  @override
+  State<HauntedScreenLayout> createState() => _HauntedScreenLayoutState();
+}
+
+class _HauntedScreenLayoutState extends State<HauntedScreenLayout> {
+  final List<SpookyObjectData> spookyObjects = [
+    SpookyObjectData(name: 'Ghost', icon: '👻', isTrap: true),
+    SpookyObjectData(name: 'Pumpkin', icon: '🎃', isTrap: true),
+    SpookyObjectData(name: 'Spider', icon: '🕷️', isTrap: true),
+    SpookyObjectData(name: 'Skull', icon: '💀', isTrap: true),
+    SpookyObjectData(name: 'Bat', icon: '🦇', isTrap: false, isWinner: true),
+  ];
+
+  final AudioPlayer _effectPlayer = AudioPlayer();
+
+  @override
+  void dispose() {
+    _effectPlayer.dispose();
+    super.dispose();
+  }
+
+  void _playTrapSound() async {
+    await _effectPlayer.stop();
+    await _effectPlayer.play(AssetSource('audio/jumpscare.mp3'));
+  }
+
+  void _playWinSound() async {
+    await _effectPlayer.stop();
+    await _effectPlayer.play(AssetSource('audio/success.mp3'));
+  }
+
+  void _handleTap(SpookyObjectData object) {
+    if (object.isTrap) {
+      _playTrapSound();
+    } else if (object.isWinner) {
+      _playWinSound();
+    }
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => OutcomeScreen(
+          isWinner: object.isWinner,
+          heroTag: object.name,
+          icon: object.icon,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFF1A1A2E),
+      ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Stack(
+            children: spookyObjects.map((object) {
+              return AnimatedSpookyObject(
+                key: ValueKey(object.name),
+                data: object,
+                constraints: constraints,
+                onTap: _handleTap,
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SpookyObjectData {
+  final String name;
+  final String icon;
+  final bool isTrap;
+  final bool isWinner;
+
+  SpookyObjectData({
+    required this.name,
+    required this.icon,
+    this.isTrap = false,
+    this.isWinner = false,
+  });
+}
+
+class AnimatedSpookyObject extends StatefulWidget {
+  final SpookyObjectData data;
+  final BoxConstraints constraints;
+  final Function(SpookyObjectData) onTap;
+
+  const AnimatedSpookyObject({
+    super.key,
+    required this.data,
+    required this.constraints,
+    required this.onTap,
+  });
+
+  @override
+  State<AnimatedSpookyObject> createState() => _AnimatedSpookyObjectState();
+}
+
+class _AnimatedSpookyObjectState extends State<AnimatedSpookyObject> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late double _top;
+  late double _left;
+  late double _rotation;
+  final double size = 60.0;
+  final double speedFactor = 0.00005;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat();
+
+    _initializePosition();
+  }
+
+  void _initializePosition() {
+    final random = Random();
+    _top = random.nextDouble() * (widget.constraints.maxHeight - size);
+    _left = random.nextDouble() * (widget.constraints.maxWidth - size);
+    _rotation = random.nextDouble() * pi * 2;
+
+    _controller.addListener(_updatePosition);
+  }
+
+  void _updatePosition() {
+    if (!mounted) return;
+    setState(() {
+      _top = _top + sin(_controller.value * 2 * pi) * 0.5;
+      _left = _left + cos(_controller.value * 2 * pi) * 0.5;
+      _rotation = _controller.value * 2 * pi;
+
+      if (_top < 0 || _top > widget.constraints.maxHeight - size) {
+        _top = _top.clamp(0.0, widget.constraints.maxHeight - size);
+      }
+      if (_left < 0 || _left > widget.constraints.maxWidth - size) {
+        _left = _left.clamp(0.0, widget.constraints.maxWidth - size);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: _top,
+      left: _left,
+      child: GestureDetector(
+        onTap: () => widget.onTap(widget.data),
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.rotate(
+              angle: _rotation,
+              child: Hero(
+                tag: widget.data.name,
+                child: Container(
+                  width: size,
+                  height: size,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: widget.data.isWinner
+                            ? const Color(0xFFFFD700).withOpacity(0.5)
+                            : Colors.purple.withOpacity(0.4),
+                        blurRadius: 8,
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    widget.data.icon,
+                    style: TextStyle(fontSize: size * 0.8),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class OutcomeScreen extends StatelessWidget {
+  final bool isWinner;
+  final String heroTag;
+  final String icon;
+
+  const OutcomeScreen({
+    super.key,
+    required this.isWinner,
+    required this.heroTag,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final String message = isWinner
+        ? 'YOU FOUND IT! The Lucky Bat!'
+        : 'SPOOKY TRAP! Try again...';
+    final Color color = isWinner ? Colors.greenAccent : Colors.redAccent;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Result'),
+        backgroundColor: const Color(0xFF2C073A),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: heroTag,
+                child: Text(
+                  icon,
+                  style: TextStyle(fontSize: 120, color: color),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Text(
+                message,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: color,
+                ),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: isWinner ? Colors.green : Colors.purple,
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: const Text('Continue Searching', style: TextStyle(fontSize: 18)),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
